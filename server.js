cad92f71655c057e3bfd9045af58f84b78d34401
ipdvonlineapp <desenvolvimento.app@ipdvonline.com.br>
2019-09-04 13:28:59 -0300

    Server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const app = require('./app');
+const hostname = '127.0.0.1';
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log(`Server running at http://${hostname}:${port}/`);
