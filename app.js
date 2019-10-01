e79baaaba124549e99e1da174f4dc5a44bbcd93b
ipdvonlineapp <desenvolvimento.app@ipdvonline.com.br>
2019-10-01 08:41:16 -0300

    routes added

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) =>{
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
+// app.use((req, res, next) =>{
+//     res.status(200).json({
+//         message: 'It works!'
+//     });
+// });
+
+const productRoutes = require("./api/routes/products");
+const ordersRoutes = require("./api/routes/orders");
+
+app.use('/products/:idProducts', productRoutes);
+app.use('/orders', ordersRoutes);
 
 module.exports = app;
