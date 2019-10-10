c69cb0ccc58a2cfbb0b2c475f9542bc28b936766
User-PC\User <alves.bernan@gmail.com>
2019-10-09 21:48:17 -0300

    add config mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const morgan = require("morgan");
 // app.use((req, res, next) =>{
 //     res.status(200).json({
 //         message: 'It works!'
@@ -10,7 +10,22 @@ const app = express();
 const productRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
 
-app.use('/products/:idProducts', productRoutes);
+app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
+
+app.use((req, res, next) =>{
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            messagem: error.message
+        }
+    });
+});
 
 module.exports = app;
